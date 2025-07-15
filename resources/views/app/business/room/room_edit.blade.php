358c923bb5e044817b22e4102b41237c990e0220
Douglas <dbnordi@gmail.com>
2025-07-15 15:29:41 -0300

    Criação das salas

---
+++ resources/views/app/business/room/room_edit.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.templates.app-layout')
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card border shadow-xs mb-4" style="height: calc(100vh - 17.5vh) !important;">
+
+                <x-card-header title="Editar Sala" action="Atualizar"></x-card-header>
+
+                 <x-form route="update" :id="$room->id">
+
+                    <div class="row">
+                        {{-- Empresa --}}
+                        <div class="col-md-6 mb-3">
+                            <label for="company_id" class="form-label">Empresa</label>
+                            <select name="company_id" id="company_id" class="form-control" required>
+                                <option value="">Selecione uma empresa</option>
+                                @foreach ($companies as $company)
+                                    <option value="{{ $company->id }}" {{ $company->id ? 'selected' : '' }}>
+                                        {{ $company->name }}
+                                    </option>
+                                @endforeach
+
+                            </select>
+                        </div>
+
+
+                        {{-- Nome do Departamento --}}
+                        <x-input col="6" set="" type="text" title="Nome da Sala" id="name"
+                            name="name" value="{{ old('name', $room->name) }}" placeholder="" />
+
+                    </div>
+
+                </x-form>
+
+            </div>
+        </div>
+    </div>
+@endsection
