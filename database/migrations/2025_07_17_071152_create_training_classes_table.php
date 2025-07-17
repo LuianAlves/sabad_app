23ff68a1a2c020065c16403ea93084168e4dd6c0
LuianAlves <luian.alves@gmail>
2025-07-17 07:23:28 -0300

    #

---
+++ database/migrations/2025_07_17_071152_create_training_classes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('training_classes', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('training_id')->constrained()->onDelete('cascade');
+            $table->foreignId('instructor_id')->constrained('employees')->onDelete('cascade');
+            $table->integer('capacity');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('training_classes');
+    }
+};
